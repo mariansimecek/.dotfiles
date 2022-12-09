894e026cc463da248d0afef26e4fed5d56f9b436
Marian Šimeček <mariansimecek@gmail.com>
2022-12-09 01:43:00 +0100

    Visual moving lines

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -1,2 +1,8 @@
 vim.g.copilot_assume_mapped = true
 vim.opt.relativenumber = true
+vim.opt.iskeyword:append("-") -- consider string-string as whole word
+
+vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>',{noremap = true, silent = true})
+vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', {noremap = true, silent = true})
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', {noremap = true, silent = true})
+vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', {noremap = true, silent = true})
--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,13 +1,18 @@
 local M = {}
 
+M.general = {
+	i = {
+		["<C-s>"] = { "<Esc> <cmd> w <CR>" },
+	},
+	v = {
+		["<C-s>"] = { "<Esc> <cmd> w <CR>gv" },
+	},
+}
+
 M.moveLine = {
-  n = {
-    ["<A-j>"] = {"<cmd> MoveLine(1)<CR>", "Move line down"},
-    ["<A-k>"] = {"<cmd> MoveLine(-1)<CR>", "Move line up"},
-  },
-  v = {
-    ["<A-j>"] = {"<cmd> MoveBlock(1)<CR>", "Move line down"},
-    ["<A-k>"] = {"<cmd> MoveBlock(-1)<CR>", "Move line up"},
-  }
+	n = {
+		["<A-j>"] = { "<cmd> MoveLine(1)<CR>", "Move line down" },
+		["<A-k>"] = { "<cmd> MoveLine(-1)<CR>", "Move line up" },
+	},
 }
 return M
